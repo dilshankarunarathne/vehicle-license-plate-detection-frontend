76afc6cf2fee2c6725d5f0b7b20cb624aadd6649
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-19 12:19:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 
+function HomePage() {
+  return (
+    <div>
+      <Link to="/scan-image">Scan Image</Link>
+      <Link to="/register-plate">Register License Plate</Link>
+    </div>
+  );
+}
+
+function ScanImage() {
+  const [image, setImage] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const handleImageUpload = (event) => {
+    setImage(event.target.files[0]);
+    // Send the image to the API and set the result
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleImageUpload} />
+      {result && <div>{JSON.stringify(result)}</div>}
+    </div>
+  );
+}
+
+function RegisterPlate() {
+  const [form, setForm] = useState({});
+
+  const handleFormChange = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Send the form to the API
+  };
+
+  return (
+    <form onChange={handleFormChange} onSubmit={handleSubmit}>
+      {/* Add form fields here */}
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          
-        </Switch>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/scan-image" element={<ScanImage />} />
+          <Route path="/register-plate" element={<RegisterPlate />} />
+        </Routes>
       </div>
     </Router>
   );
